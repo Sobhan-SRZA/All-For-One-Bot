b33c7a1f9e4d98b7fbb4596126335301a51863b4
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2021-12-01 21:14:05 +0330

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,16 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: '',
+        prefix: '-',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
