9ffa9c43608b00ecc3c0bd6dc78acaa130a037e2
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2022-12-14 01:48:13 +0330

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,5 +2,5 @@ echo off
 color 1
 cls
 :a
-npm install
+npm install @discordjs/opus akaneko aki-api axios booru canvas cli-color cpu-stat dateformat discord-buttons discord-giveaways discord-player discord.js@^12 easy-games-js express got mathjs moment ms nekos.life node-fetch novelcovid pornsearch quick.db random-puppy request-promise-native rss-parser snakecord snekfetch sqlite srod-v2 superagent
 goto a
