30b27b05511518611d45b8f10994656fa3848a57
kabboiTheDev <161407420+kabboiTheDev@users.noreply.github.com>
2024-03-03 10:56:26 +0600

    Update index.js
    webviwe :)

--- index.js
+++ index.js
@@ -35,6 +35,25 @@
 /**
  * Require All Packages
  */
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`
+    <html>
+      <head>
+        <title>Your Web View</title>
+      </head>
+      <body style="margin: 0; padding: 0;">
+        <iframe width="100%" height="100%" src="https://axocoder.vercel.app/" frameborder="0" allowfullscreen></iframe>
+      </body>
+    </html>`);
+});
+
+server.listen(3000, () => {
+  console.log('Server Online because of Axo Coder ✅!!');
+});
+// made for webviwe lol
 require("dotenv").config();
 const fs = require("fs");
 const Discord = require("discord.js");
